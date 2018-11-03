a56a122bea06ae715fa2339eedde7a204f0a284d
Thomas J. Webb <thomas@osakared.com>
2018-11-03 07:01:01 -0700

    Updated MidiIn interface

--- src/grig/midi/MidiIn.hx
+++ src/grig/midi/MidiIn.hx
@@ -4,4 +4,10 @@ extern class MidiIn
 {
     public function new();
     public function getPorts():Array<String>;
+    public function openPort(portNumber:Int, portName:String):Void;
+    public function openVirtualPort(portName:String):Void;
+    public function closePort():Void;
+    public function isPortOpen():Bool;
+    public function setCallback((MidiMessage, float)->Void):Void;
+    public function cancelCallback():Void;
 }
